9a70a68565a04e725bf20949d85d673d2f83d626
ketulkumar <k2lsuthar@gmail.com>
2020-04-30 17:02:12 -0400

    added code for taking input from user and called getAverageOfOdd for calculating average.

--- src/masteringhashmap/Main.java
+++ src/masteringhashmap/Main.java
@@ -1,10 +1,25 @@
 package masteringhashmap;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		HashMap<Integer,Integer> hmap = new HashMap<Integer,Integer>();
+		
+		for(int i = 0; i < n; i++) {
+			
+			hmap.put(sc.nextInt(), sc.nextInt());
+		}
+		
+		System.out.println(UserMainCode.getAverageOfOdd(hmap));
+		
+		sc.close();
 	}
 
 }
