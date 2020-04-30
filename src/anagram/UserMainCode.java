279b5bb1c59d2e06b2d4d735fe447d53e01d865f
ketulkumar <k2lsuthar@gmail.com>
2020-04-30 17:28:26 -0400

    Implemented code in main class for yaking inpot from user and output.

--- src/anagram/UserMainCode.java
+++ src/anagram/UserMainCode.java
@@ -1,5 +1,23 @@
 package anagram;
 
+import java.util.Arrays;
+
 public class UserMainCode {
 
+	public static int getAnagram(String s1, String s2) {
+		
+		s1 = s1.replace(" ", "");
+		s2 = s2.replace(" ", "");
+		
+		char s1A[] = s1.toCharArray();
+		char s2A[] = s2.toCharArray();
+		
+		Arrays.sort(s1A);
+		Arrays.sort(s2A);
+		
+		if (new String(s1A).equals(new String(s2A))) {
+			return 1;
+		}
+		return -1;
+	}
 }
