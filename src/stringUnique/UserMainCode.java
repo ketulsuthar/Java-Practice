e2f6df3304fbea60800afdb7d25a8a266d672c9a
ketulkumar <k2lsuthar@gmail.com>
2020-04-30 13:53:38 -0400

    Added logic for String manipulation.

--- src/stringUnique/UserMainCode.java
+++ src/stringUnique/UserMainCode.java
@@ -2,4 +2,24 @@ package stringUnique;
 
 public class UserMainCode {
 
+	public static String replacePlus(String str1, String str2) {
+		
+		String s1 = str1.toLowerCase();
+		String s2 = str2.toLowerCase();
+		
+		StringBuffer sb = new StringBuffer();
+		
+		for(int i = 0; i < str1.length(); i++) {
+			
+			char c = s1.charAt(i);
+			if(s2.indexOf(c)== -1) {
+				sb.append('+');
+			}else {
+				sb.append(str1.charAt(i));
+			}
+		}
+		
+		
+		return sb.toString();
+	}
 }
