ff326f32a238eb19705d51cc3a23f99ce100491d
ketulkumar <k2lsuthar@gmail.com>
2020-04-30 15:42:32 -0400

    Addded code for taking input from user and called function for processing input.

--- src/stringoccurences/Main.java
+++ src/stringoccurences/Main.java
@@ -1,8 +1,21 @@
 package stringoccurences;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		String str1 = sc.nextLine();
+		String str2 = sc.nextLine();
+		
+		
+		System.out.println(UserMainCode.countNoOfWords(str1, str2));
+		
+
+		sc.close();
 		
 	}
 
