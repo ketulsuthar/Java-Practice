526735cde20ddfb724fe83ea60b33b52361b5ab4
ketulkumar <k2lsuthar@gmail.com>
2020-04-30 17:02:52 -0400

    added code for calculating avarage if key is odd.

--- src/masteringhashmap/UserMainCode.java
+++ src/masteringhashmap/UserMainCode.java
@@ -1,5 +1,22 @@
 package masteringhashmap;
 
+import java.util.HashMap;
+import java.util.Iterator;
+
 public class UserMainCode {
 
+	public static int getAverageOfOdd(HashMap<Integer,Integer> hmap) {
+		
+		Iterator<Integer> itr = hmap.keySet().iterator();
+		int total,count;
+		total=count=0;
+		while(itr.hasNext()) {
+			int key = itr.next();
+			if (key %2 != 0) {
+				count++;
+				total+=hmap.get(key);
+			}
+		}
+		return total/count;
+	}
 }
