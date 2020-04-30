667be3700259dc3d224afa1c4a512b764ea50891
ketulkumar <k2lsuthar@gmail.com>
2020-04-30 15:58:40 -0400

    Added logic for counting words from first string if it's match with secord word of second string.

--- src/stringoccurences/UserMainCode.java
+++ src/stringoccurences/UserMainCode.java
@@ -2,4 +2,18 @@ package stringoccurences;
 
 public class UserMainCode {
 
+	public static int countNoOfWords(String str1, String str2) {
+		
+		String[] s1 = str1.split(" ");
+		String[] s2 = str2.split(" ");
+		
+		int count = 0;
+		for(int i = 0; i < s1.length;i++) {
+			
+				if(s1[i].equals(s2[1]))
+					count++;
+		}
+		
+		return count;
+	}
 }
