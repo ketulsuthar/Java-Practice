346f3b7a9d654474d17b00202de8759425f9812e
ketulkumar <k2lsuthar@gmail.com>
2020-04-30 16:34:24 -0400

    Implement code for gerenerating oddeven arraylist from 2 arraylist.

--- src/arraylistmanipulation/UserMainCode.java
+++ src/arraylistmanipulation/UserMainCode.java
@@ -1,5 +1,20 @@
 package arraylistmanipulation;
 
+import java.util.ArrayList;
+
 public class UserMainCode {
 
+	public static ArrayList generateOddEvenList(ArrayList<Integer> a1, ArrayList<Integer> a2) {
+		ArrayList<Integer> a3 = new ArrayList<Integer>();
+		
+		for(int i =0; i < a1.size(); i++) {
+			if (i%2 ==0) {
+				a3.add(a2.get(i));
+			}else {
+				a3.add(a1.get(i));
+			}
+		}
+		
+		return a3;
+	}
 }
