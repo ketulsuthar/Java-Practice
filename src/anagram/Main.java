bf08eb0cc9a13abad125c08f747181257367ff0e
ketulkumar <k2lsuthar@gmail.com>
2020-04-30 17:28:19 -0400

    Implemented code in main class for yaking inpot from user and output.

--- src/anagram/Main.java
+++ src/anagram/Main.java
@@ -1,10 +1,26 @@
 package anagram;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-
+		Scanner sc = new Scanner(System.in);
+		
+		String s1 = sc.nextLine();
+		String s2 = sc.nextLine();
+		
+		if(UserMainCode.getAnagram(s1, s2) < 0) {
+			
+			System.out.println("Not Anagram");
+			
+		}else {
+			
+			System.out.println("Anagram");
+			
+		}
+		
+		sc.close();
 	}
 
 }
