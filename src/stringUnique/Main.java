2e432aac613b5befb3d5816619292fe21959155b
ketulkumar <k2lsuthar@gmail.com>
2020-04-30 13:43:15 -0400

    Implemented logic for taking input from logic and called methos for manipulation string.

--- src/stringUnique/Main.java
+++ src/stringUnique/Main.java
@@ -1,9 +1,16 @@
 package stringUnique;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
+		String str1 = sc.nextLine();
+		String str2 = sc.nextLine();
+		
+		System.out.println(UserMainCode.replacePlus(str1, str2));
 
 	}
 
