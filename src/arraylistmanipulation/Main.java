628d8f9b805c99a093453d5415741dab246d5161
ketulkumar <k2lsuthar@gmail.com>
2020-04-30 16:33:34 -0400

    Added code for taking input from user and called generateOddEvenList function for generating arraylist.

--- src/arraylistmanipulation/Main.java
+++ src/arraylistmanipulation/Main.java
@@ -1,10 +1,32 @@
 package arraylistmanipulation;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		ArrayList<Integer> array1 = new ArrayList<Integer>();
+		ArrayList<Integer> array2 = new ArrayList<Integer>();
+		
+		
+		int n = sc.nextInt();
+		for(int i=0; i<n;i++) {
+			array1.add(sc.nextInt());
+		}
+		for(int i=0; i<n;i++) {
+			array2.add(sc.nextInt());
+		}
+		
+		for(Object i :UserMainCode.generateOddEvenList(array1,array2)) {
+			
+			System.out.println(i);
+		}
+		
+		
+		sc.close();
 	}
 
 }
